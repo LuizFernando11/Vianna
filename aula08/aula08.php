c1779901ea2f3d5601febe93f77386416d46cbfd
LuizFernando11 <61070020+LuizFernando11@users.noreply.github.com>
2023-03-13 21:13:46 -0300

    Add files via upload

---
+++ aula08/aula08.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 08</title>
+</head>
+<body>
+
+    <form method="post" action="processar.php">
+        <div class="form-group">
+            <label for="nome">Nome: </label>
+            <input type="text" name="nome" id="nome">
+        </div>
+        <div class="form-group">
+            <label for="periodo">Periodo: </label>
+            <input type="text" name="periodo" id="periodo">
+        </div>
+
+        <input type="submit" value="Enviar">
+    </form>
+
+
+    
+</body>
+</html>
